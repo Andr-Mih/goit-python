8bcf19dba4a85f86dffd60fe887e7b12bd96caaf
Andr-Mih <amihalkin@gmail.com>
2020-12-19 20:15:21 +0200

    Home Work2

--- lesson2/hw_3.py
+++ lesson2/hw_3.py
@@ -4,10 +4,14 @@ while True:
     try:
         val_1 = int(val_1)
     except ValueError:
+
         print(f'Value {val_1} is not number')
+
         val_1 = input('Enter number 1 again: ')
+
     else:
         break
+
 print(val_1)
 
 # Enter operand
@@ -15,26 +19,43 @@ operation = input('Enter type of operation: ')
 while True:
     if operation == '+' or operation == '-' or operation == '*' or operation == '/':
         break
+
     print('Is not correct operation ')
+
     operation = input('Enter type of operation: ')
+
 print (val_1, operation)
 
 # Enter number 2
 val_2 = input('Enter number 2 : ')
+# Zero Division check
+while True:
+    if operation == '/' and val_2 == '0':
+        print('Zero division')
+        val_2 = input('Enter number 2 again: ')
+    else:
+        break
+
 while True:
     try:
         val_2 = int(val_2)
     except ValueError:
+
         print(f'Value {val_2} is not number')
-        val_2 = input('Enter number 2 again: ')
+
+        val_2 = input('Enter number 2 again: ') 
     else:
         break
+
+
 print(val_1, operation, val_2)
 
 # Submit the calculation
 user_done = input('Plese enter = for calculate: ')
 while user_done != '=':
+
     print('Incorrect')
+
     user_done = input('Plese enter = for calculate: ')
 
 # Calculation
