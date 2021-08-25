487cc41a3d2ee11de6a1acadee7bf0643e5efa80
Andr-Mih <connet987@gmail.com>
2021-08-25 22:02:49 +0300

    HomeWork14

---
+++ Block2/Modul14/test_spyder/test_spyder/spiders/authors.py
@@ -0,0 +1,20 @@
+import scrapy
+
+
+class AuthorsSpider(scrapy.Spider):
+    name = 'authors'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        for quote in response.xpath("/html//div[@class='quote']"):
+            a = quote.xpath("span[@class='text']/text()").get()
+            b = a[1:-2]
+            yield {
+                "keywords": quote.xpath("div[@class='tags']/a/text()").extract(),
+                "author": quote.xpath("span/small/text()").extract(),
+                "quote": b
+            }
+        next_link = response.xpath("//li[@class='next']/a/@href").get()
+        if next_link:
+            yield scrapy.Request(url=self.start_urls[0] + next_link)
